2758483d98ce8b400bf069795f529b54dce256c4
Irianto <irianto223@gmail.com>
2017-11-07 14:18:21 +0700

    on/off button done

--- yota_driver/src/screens/Home.js
+++ yota_driver/src/screens/Home.js
@@ -18,24 +18,13 @@ class Home extends React.Component {
   constructor() {
     super()
     this.state = {
+      date: JSON.stringify(new Date()),
       latitude: 0,
       longitude: 0,
       error: null,
-      car_id: '59fda4375254c414ec370484'
+      car_id: '59fda4375254c414ec370484',
+      isActive: false
     }
-  }
-
-  render() {
-    return (
-      <View>
-        <TouchableHighlight onPress={ () => this.postCurrentPosition() }>
-          <Text>TAP HER TO POST!</Text>
-        </TouchableHighlight>
-        <Text>car_id: {this.state.car_id}</Text>
-      <Text>latitude: {this.state.latitude}</Text>
-    <Text>longitude: {this.state.longitude}</Text>
-      </View>
-    )
   }
 
   componentDidMount() {
@@ -44,26 +33,71 @@ class Home extends React.Component {
     // this.watchPosition()
   }
 
+  render() {
+    return (
+      <View>
+        <Text>is active: { JSON.stringify(this.state.isActive) }</Text>
+        <Text>date/time: { this.state.date }</Text>
+        <Text>car_id: {this.state.car_id}</Text>
+        <Text>latitude: {this.state.latitude}</Text>
+        <Text>longitude: {this.state.longitude}</Text>
+        { this.showActivateButton() }
+      </View>
+    )
+  }
+
+  showActivateButton() {
+    if (this.state.isActive) {
+      return (
+        <Button
+          onPress={() => {
+            return (
+              this.toggleStatusActive()
+            )
+          }}
+          title="disable"
+          color="red"
+        />
+      )
+    } else {
+      return (
+        <Button
+          onPress={() => {
+            return (
+              this.toggleStatusActive()
+            )
+          }}
+          title="enable"
+          color="green"
+        />
+      )
+    }
+  }
+
+  toggleStatusActive() {
+    this.setState({isActive: !this.state.isActive})
+  }
+
   watchSpeed() {
     const accelerationObservable = new Accelerometer({
-      updateInterval: 10000 // defaults to 100ms
+      updateInterval: 5000 // defaults to 100ms
     })
-
-    let lat = this.state.latitude
-    let lng = this.state.longitude
 
     // Normal RxJS functions
     accelerationObservable
       .map(({ x, y, z }) => x + y + z)
       .filter(speed => speed < 10)
       .subscribe(speed => {
-        this.getCoordinate()
-        // this.postCurrentPosition()
-        console.log(`You moved your phone with ${speed}`)
+        if (this.state.isActive) {
+          console.log(`You moved your phone with ${speed}`)
+          this.getCoordinate()
+          this.postCurrentPosition()
+        }
       });
 
     // setTimeout(() => {
     //   accelerationObservable.stop();
+    //   console.log('acc.stop() run');
     // }, 1000);
   }
 
@@ -88,13 +122,13 @@ class Home extends React.Component {
   getCoordinate() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log('posisi sekarang:', position)
+        console.log('state sekarang:', this.state)
         // alert(JSON.stringify(position))
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           error: null
-        });
+        })
       },
       (error) => {
         // alert(JSON.stringify(error))
@@ -105,6 +139,7 @@ class Home extends React.Component {
   }
 
   postCurrentPosition() {
+    this.getCoordinate()
     axios({
       method: 'post',
       url: `http://yota.achim.my.id/positions`,
@@ -115,12 +150,12 @@ class Home extends React.Component {
       }
     })
     .then(response => {
-      console.log(response)
-      // alert(JSON.stringify(response, null, 2))
+      console.log('response axios:', response.data)
+      alert(JSON.stringify(response.data, null, 2))
     })
     .catch(err => {
-      console.log(err)
-      // alert(JSON.stringify(err, null, 2))
+      console.log('error axios:', err)
+      alert(JSON.stringify(err, null, 2))
     })
   }
 }
